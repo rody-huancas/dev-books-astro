bdbc17acf677f1901bfb6e4d27b557ef2d03cb81
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-04 16:13:14 -0500

    Feat: add environment variables for buy button and score API, implement BookScore and AmazonLogo components, and enable prerendering for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON   : envField.boolean({ default: true, context: "server", access: "public" }),
+      SCORE_API_ENDPOINT: envField.string({ context: "server", access: "public" }),
+    },
+  },
 });
